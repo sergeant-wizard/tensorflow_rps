859111c21ee17a51ed4f7967d12f1e1b7db9de28
Ryo Miyajima <r.miyajima@aktsk.jp>
2015-11-11 17:24:42 +0900

    remove summary

--- main.py
+++ main.py
@@ -23,9 +23,6 @@ y_ = tf.placeholder("float", [None, 3])
 cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
 
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-tf.scalar_summary("entropy", cross_entropy)
-summary_writer = tf.train.SummaryWriter('data')
-summary_op = tf.merge_all_summaries()
 
 init = tf.initialize_all_variables()
 feed_dict={x: input, y_: winning_hands}
@@ -35,7 +32,4 @@ with tf.Session() as sess:
 
   for step in range(1000):
     sess.run(train_step, feed_dict=feed_dict)
-    if step % 100 == 0:
-      summary_str = sess.run(summary_op, feed_dict=feed_dict)
-      summary_writer.add_summary(summary_str, step)
 
